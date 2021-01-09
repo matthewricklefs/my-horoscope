b415b31e97cff984d506c4fb8a0f898f3db4b1fd
matthewricklefs <ricklefsmatthew1003@gmail.com>
2021-01-09 13:35:05 -0500

    fixed button in SelectTimeframe.jsx

--- src/components/SelectTimeframe.jsx
+++ src/components/SelectTimeframe.jsx
@@ -17,6 +17,7 @@ export const SelectTimeframe = ({
                             onTimeframeSelected(timeframes) 
                         }  
                         > 
+                        {timeframes}
                         </button>
                 ))}
             </div>
