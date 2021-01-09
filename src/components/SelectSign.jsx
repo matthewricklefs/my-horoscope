ba54ac5f1fa0922e1616f7d21e44a8b2dfd08315
matthewricklefs <ricklefsmatthew1003@gmail.com>
2021-01-09 13:11:15 -0500

    fixed all bugs in console, application is functional, needing to fix the timeframe button sizing

--- src/components/SelectSign.jsx
+++ src/components/SelectSign.jsx
@@ -1,35 +1,30 @@
-import React, { useState, useEffect } from 'react'
-import { getSigns } from '../services/api'
+import React, { useState, useEffect } from "react";
+import { getSigns } from "../services/api";
 
 export const SelectSign = ({ onSignSelected }) => {
-    
-    const [signs, setSigns ] = useState([])
-    const [selectedSign, setSelectedSign] = useState([])
+  const [signs, setSigns] = useState([]);
 
-    useEffect( () => {
-        getSigns().then(setSigns)   
-      }, [] )
-    
-    return (
-       <> 
-            <h2>Please select a sign</h2>
+  useEffect(() => {
+    getSigns().then(setSigns);
+  }, []);
 
-            {selectedSign}
+  return (
+    <>
+      <h2>Please select a sign</h2>
 
-            <div className='grid'>
-                {signs.map((sign) => (
-                    <button 
-                    className='sign'
-                    key={sign}
-                    onClick={ () => setSelectedSign(sign) }  
-                    > 
+      <div className="grid">
+        {signs.map((sign) => (
+          <button
+            className="sign"
+            key={sign}
+            onClick={() => onSignSelected(sign)}
+          >
+            {sign}
+          </button>
+        ))}
 
-                    {sign} 
-                    </button>
-                ))}
-
-                { onSignSelected }
-            </div>
-        </>
-    )
-}
+        {onSignSelected}
+      </div>
+    </>
+  );
+};
